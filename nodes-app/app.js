765d4372709160fa6a3e1c8b0691045a34cf3de7
ithar <itharm@ext.inditex.com>
2020-03-28 22:48:03 +0000

    Added functionality to add and list notes.

--- nodes-app/app.js
+++ nodes-app/app.js
@@ -1,15 +1,32 @@
 const validator = require('validator');
 const yargs = require('yargs');
 const chalk = require('chalk');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 yargs.version('1.1.0');
 
 yargs.command({
     command: 'ADD', 
     describe: 'Add a new note',
-    handler: function() {
-        console.log(chalk.green('New note added successfully'));
+    builder: {
+        title: {
+            type: 'string',
+            demandOption: true,
+            describe: 'Note title'
+        }, 
+        body: {
+            type: 'string',
+            demandOption: true,
+            describe: 'Note body'
+        }        
+    },
+    handler: function(argv) {
+        const success = notes.addNote(argv.title, argv.body)
+        if (success) {
+            console.log(chalk.green('New note added successfully'));
+        } else {
+            console.log(chalk.gray('Failed to add note title "' + argv.title +'"  already exists'));
+        }
     }
 });
 
@@ -25,8 +42,16 @@ yargs.command({
     command: 'LIST', 
     describe: 'List notes',
     handler: function() {
-        console.log(chalk.blue(getNotes()));
+
+        const myNotes = notes.getNotes();
+
+        console.log(chalk.blue('Your notes ('+myNotes.length+') ...'));
         
+        var i = 1;
+        myNotes.forEach(note => {
+            console.log(chalk.blue(i + ':\t' +note.title)); 
+            i++
+        });
     }
 });
 
@@ -34,22 +59,8 @@ yargs.command({
     command: 'READ', 
     describe: 'Reading notes',
     handler: function() {
-        console.log(chalk.white('Reading your notes'));
+        console.log(chalk.white('Reading you a note'));
     }
 });
 
-
-console.log(yargs.argv);
-
-// const command = yargs.argv[1];
-// const title = yargs.argv.title;
-// const body = yargs.argv.body;
-
-// console.log('COMMAND:' + command + '\n[title='+title+', body='+body+']');
-
-// if (command === 'ADD') {
-//     console.log(chalk.green('Add note'));
-// } else if (command === 'REMOVE') {
-//     console.log(chalk.red('Deleting a note'));
-// }
-
+yargs.parse();
--- nodes-app/notes.js
+++ nodes-app/notes.js
@@ -1,5 +1,56 @@
-const getNotes = function() {
-  return 'Your notes ...';
-};
+const chalk = require('chalk');
+const fs = require('fs');
 
-module.exports = getNotes;
+const notesFile = 'notes.json';
+
+const notes = {
+
+  getNotes : function() {
+    const notes = loadNotes();
+    return notes;
+  }, 
+  addNote : function(title, body) {
+
+    const notes = loadNotes();
+
+    let dupTitle = false; 
+    notes.forEach(note => { 
+      if (note.title === title) {
+        dupTitle = true;
+      }  
+    });
+
+    if (!dupTitle) {
+      notes.push(createNote(title, body));
+      saveNotes(notes);
+      return true;
+    } 
+
+    return false;
+  }
+
+}
+
+const createNote = function(title, body) {
+    const note = {
+      title : title,
+      body: body
+  };
+
+  return note;
+}
+
+const loadNotes = function() {
+  try {
+    return JSON.parse(fs.readFileSync(notesFile).toString());
+  } catch(e) {
+    console.log(chalk.red.bold('Error ' +e.message));
+    return [];
+  }  
+}
+
+const saveNotes = function(notes) {
+  fs.writeFileSync(notesFile, JSON.stringify(notes));
+}
+
+module.exports = notes;
